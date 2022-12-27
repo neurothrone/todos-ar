0586f6312c818d79b043dc603e1a47a27c8ee772
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-12-27 21:49:46 +0100

    Initial Commit

---
+++ TodosAR/TodosARApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodosARApp.swift
+//  TodosAR
+//
+//  Created by Zaid Neurothrone on 2022-12-27.
+//
+
+import SwiftUI
+
+@main
+struct TodosARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
