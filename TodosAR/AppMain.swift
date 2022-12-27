e58e84f6932dc4a333c5a6ef89833b51a8dc19f5
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-12-27 21:54:43 +0100

    set up project for AR

---
+++ TodosAR/AppMain.swift
@@ -0,0 +1,17 @@
+//
+//  AppMain.swift
+//  TodosAR
+//
+//  Created by Zaid Neurothrone on 2022-12-27.
+//
+
+import SwiftUI
+
+@main
+struct AppMain: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
--- TodosAR/ContentView.swift
+++ TodosAR/ContentView.swift
@@ -5,22 +5,29 @@
 //  Created by Zaid Neurothrone on 2022-12-27.
 //
 
+import RealityKit
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+  var body: some View {
+    ZStack {
+      ARViewContainer()
+        .edgesIgnoringSafeArea(.all)
     }
+  }
+}
+
+struct ARViewContainer: UIViewRepresentable {
+  func makeUIView(context: Context) -> ARView {
+    let arView = ARView(frame: .zero)
+    return arView
+  }
+  
+  func updateUIView(_ uiView: ARView, context: Context) {}
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
